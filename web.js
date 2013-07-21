7885fe5665d83fef5011fa3ff31c37dd4a6d48eb
Robby Cahyadi Hendro Saputro <robbychs@gmail.com>
2013-07-21 02:54:55 +0000

    hw4 part1

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html')
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
